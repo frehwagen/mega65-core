8594b39be9d777149a2b0af693373c1b74a9f8d7
Falk Rehwagen <frehwagen@bluewaysw.de>
2019-10-11 00:33:36 +0200

    Try to prevent double execution of vdc write.

--- src/vhdl/gs4510.vhdl
+++ src/vhdl/gs4510.vhdl
@@ -537,7 +537,9 @@ architecture Behavioural of gs4510 is
   signal nmi_state : std_logic := '1';
   signal no_interrupt : std_logic := '0';
   signal hyper_trap_last : std_logic := '0';
+  signal hyper_trap_vdc_data_last : std_logic := '0';
   signal hyper_trap_edge : std_logic := '0';
+  signal hyper_trap_vdc_data_edge : std_logic := '0';
   signal hyper_trap_pending : std_logic := '0';
   signal hyper_trap_state : std_logic := '1';
   signal matrix_trap_pending : std_logic := '0';
@@ -3266,8 +3268,14 @@ begin
       else
         hyper_trap_edge <= '0';
       end if;
+      if hyper_trap_vdc_data = '1' and hyper_trap_vdc_data_last = '0' then
+        hyper_trap_vdc_data_edge <= '1';
+      else
+        hyper_trap_vdc_data_edge <= '0';
+      end if;
       hyper_trap_last <= hyper_trap;
-      if (hyper_trap_edge = '1' or matrix_trap_in ='1' or hyper_trap_f011_read = '1' or hyper_trap_f011_write = '1' or hyper_trap_vdc_reg = '1' or hyper_trap_vdc_data = '1' or hyper_trap_vdc_data_read = '1')
+      hyper_trap_vdc_data_last <= hyper_trap_vdc_data;
+      if (hyper_trap_edge = '1' or matrix_trap_in ='1' or hyper_trap_f011_read = '1' or hyper_trap_f011_write = '1' or hyper_trap_vdc_reg = '1' or hyper_trap_vdc_data_edge = '1' or hyper_trap_vdc_data_read = '1')
         and hyper_trap_state = '1' then
         hyper_trap_state <= '0';
         hyper_trap_pending <= '1'; 
