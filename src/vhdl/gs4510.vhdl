c8a1cc9b9ca770288f4f6cdcc15fc27ee7b8a2d3
Falk Rehwagen <frehwagen@bluewaysw.de>
2026-04-25 10:50:43 +0200

    Actually connect the slow device section.

--- src/vhdl/gs4510.vhdl
+++ src/vhdl/gs4510.vhdl
@@ -3196,8 +3196,7 @@ begin
           return unsigned(hyppo_rdata);
         when SlowRAM =>
           report "reading slow RAM data. Word is $" & to_hstring(slow_access_rdata) severity note;
-          return x"11";
-          --unsigned(slow_access_rdata);
+          return unsigned(slow_access_rdata);
         when SlowRAMPreFetch =>
           report "reading slow prefetched RAM data. byte is $" & to_hstring(slow_prefetch_data) severity note;
           report "(last CACHE byte read is $" & to_hexstring(cache_line_last_data_read) & ")";
