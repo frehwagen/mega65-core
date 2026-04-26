bcf847aaf9ee09642bdb8d3b66170c45672db92e
Falk Rehwagen <frehwagen@bluewaysw.de>
2026-04-26 18:59:54 +0200

    More preferch debugging.

--- src/vhdl/slow_devices.vhdl
+++ src/vhdl/slow_devices.vhdl
@@ -567,7 +567,8 @@ begin
             & " due to slow access read that had to ask HyperRAM for data.";
           slow_prefetched_address(26 downto 3) <= expansionram_current_cache_line_address;
           slow_prefetched_address(2 downto 0) <= slow_access_address(2 downto 0)+1;
-          slow_prefetched_data <= expansionram_current_cache_line(to_integer(slow_access_address(2 downto 0))+1);
+          slow_prefetched_data <= x"BB";
+          --expansionram_current_cache_line(to_integer(slow_access_address(2 downto 0))+1);
         end if;
 -- Don't retry, as this can result in an infinite loop. If we don't get a
 -- response within the allowed time, it's just aborted.
